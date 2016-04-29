c363a10ce758d30af22e1a5b30eeb54ff4e4bf52
InsZVA <inszva@126.com>
2016-04-29 11:26:52 +0800

    fix bugs

--- sql/syntax/calculate.go
+++ sql/syntax/calculate.go
@@ -1,15 +1,28 @@
 package syntax
 
 import (
-    "strconv"
+	"strconv"
 )
 
 func calculateInt(raw []byte) int {
-    i, _ := strconv.Atoi(string(raw))
-    return i
+	i, err := strconv.Atoi(string(raw))
+	if err != nil {
+		return 0
+	}
+	return i
 }
 
 func calculateFloat(raw []byte) float64 {
-    f, _ := strconv.ParseFloat(string(raw), 64)
-    return f
+	f, err := strconv.ParseFloat(string(raw), 64)
+	if err != nil {
+		fI, err := (strconv.Atoi(string(raw)))
+
+		if err != nil {
+			return 0.0
+		} else {
+			f = float64(fI)
+			return f
+		}
+	}
+	return f
 }
